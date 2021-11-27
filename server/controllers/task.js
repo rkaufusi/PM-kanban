177e5687556f1fae619b7a09d62a8ac0e7114f3e
rkaufusi <rhettkaufusi@gmail.com>
2021-11-26 18:31:06 -0800

    server side setup incl. CREATE

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -1,7 +1,21 @@
-export const getTasks = (req, res) => {
-    res.send('Router is working');
+export const getTasks = async (req, res) => {
+    try {
+        const allTasks = await task.find();
+
+        res.status(200).json(allTasks);
+    } catch (error) {
+        res.status(404).json({message: error.message})
+    }
 };
 
-export const createTask = (req, res) => {
+export const createTask = async (req, res) => {
+    const task = req.body;
+    const newTask = new taskModel(task);
+    try {
+        await newTask.save();
+        res.status(201).json(newTask);
+    } catch (error) {
+        res.status(409).json({message: error.message})
+    }
     res.send('Router is working');
 };
