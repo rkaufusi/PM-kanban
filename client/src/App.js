8e5561fcc32f5f87ad4f6e6f34dcd60dfad9355a
rkaufusi <rhettkaufusi@gmail.com>
2021-11-30 17:09:33 -0800

    created front end layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
+import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import './App.css';
+import Task from './components/showTask/showTask.js';
+import CreateTask from './components/createTask/createTask.js'
+import useStyles from './styles.js'
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Project Manager</h1>
-      </header>
+      <Container maxWidth="lg">
+        <AppBar className={classes.AppBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">Tasks</Typography>
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stetch">
+              <Grid item xs={12} sm={7}>
+                <AppBar className={classes.appBar} position="static" color="inherit">
+                  <Task />
+                </AppBar>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <AppBar className={classes.appBar} position="static" color="inherit">
+                  <CreateTask />
+                </AppBar>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
